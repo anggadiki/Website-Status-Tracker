590aa9080f61ff470ea23f26ca42904c74d84c9b
angga diki <anggadikisaputraagung@gmail.com>
2025-07-27 12:58:15 +0700

    feat: add allowed cross origin in dev modes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  allowedDevOrigins: ["http://192.168.1.21:3000", "http://localhost:3000"],
+};
+
 export default nextConfig;
